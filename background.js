ec195b17027c7fa081c71c758a828fea8c5cc130
neal <neal1991@sina.com>
2019-06-20 17:40:01 +0800

    use fetch to send request

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     (details) => {
         console.dir(details)
+        fetch('http://127.0.0.1')
     },
     { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking', 'extraHeaders']);
