0d5af7f268937c2d79dca24254e2946f8afb2d18
neal1991 <neal1991@sina.com>
2019-06-22 13:37:43 +0800

    add sample js

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//     (details) => {
+//         console.dir(details)
+//         fetch('http://127.0.0.1')
+//     },
+//     { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking', 'extraHeaders']);
+
+chrome.webRequest.onCompleted.addListener(
     (details) => {
         console.dir(details)
         fetch('http://127.0.0.1')
-    },
-    { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking', 'extraHeaders']);
+    }, { urls: ["<all_urls>"] });
+
+function setListeners() {
+    chrome.webRequest.onBeforeSendHeaders.addListener(requestHandler,
+        { urls: ["<all_urls>"] },
+        ["requestHeaders"]);
+    chrome.webRequest.onBeforeRequest.addListener(requestHandler,
+        { urls: ["<all_urls>"] },
+        ['requestBody']);
+    chrome.webRequest.onCompleted.addListener(requestHandler,
+        { urls: ["<all_urls>"] });
+}
