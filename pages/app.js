0a6b98d06821744b5b27def3e0885d954cf80a8c
neal <neal1991@sina.com>
2019-06-24 17:13:35 +0800

    add fuch react js

---
+++ pages/app.js
@@ -0,0 +1,18 @@
+// import React from 
+// import ReactDOM 
+
+class App extends React.Component {
+    render() {
+        return (
+            <button className="square">
+               134
+            </button>
+        );
+    }
+}
+
+const ap  = "qerqewr";
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
