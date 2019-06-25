329cf3c39c780a07a6d2dfb41d099617c092cf19
neal <neal1991@sina.com>
2019-06-25 11:24:55 +0800

    should notice async methods

--- background.js
+++ background.js
@@ -1,35 +1,56 @@
 // All resource types
-// "main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", 
+// "main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object",
 //"xmlhttprequest", "ping", "csp_report", "media", "websocket", or "other"
-const resourceTypes = ["main_frame", "sub_frame", "object",
- "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"];
-const requestFilters = {
-    urls: ["<all_urls>"],
-    types: resourceTypes
-}
+const resourceTypes = [
+  "main_frame",
+  "sub_frame",
+  "object",
+  "xmlhttprequest",
+  "ping",
+  "csp_report",
+  "media",
+  "websocket",
+  "other"
+];
+let requestFilters = {
+  urls: ["<all_urls>"],
+  types: ["main_frame"]
+};
 
-const BadgeText = {
-    ERROR = 'Error',
-    OFF = 'OFF',
-    ON = 'ON',
-}
+chrome.storage.local.get("types", function(result) {
+  requestFilters["types"] = result.types;
+  chrome.webRequest.onSendHeaders.addListener(
+    details => {
+      // method = details.method;
+      console.log("filters");
+      console.dir(requestFilters.types);
+      rHeaders = details.requestHeaders;
+      console.log("sendHeaders");
+      console.dir(rHeaders);
+      type = details.type;
+      console.log(type);
+      // timeStamp = details.timeStamp;
+      // url = details.url;
+      console.dir(details);
+    },
+    requestFilters,
+    ["requestHeaders", "extraHeaders"]
+  );
+});
 
 function setIcon(config) {
-    if (config['control'] === BadgeText.OFF) {
-
-    }
+  if (config["control"] === BadgeText.OFF) {
+  }
 }
-
 
 function setBadgeAndBackgroundColor(text, color) {
-    chrome.browserAction.setBadgeText({
-        text: text
-    });
-    chrome.browserAction.setBackgroundColor({
-        color: color
-    });
+  chrome.browserAction.setBadgeText({
+    text: text
+  });
+  chrome.browserAction.setBackgroundColor({
+    color: color
+  });
 }
-
 
 // chrome.webRequest.onBeforeSendHeaders.addListener(
 //     (details) => {
@@ -45,23 +66,6 @@ function setBadgeAndBackgroundColor(text, color) {
 //         console.dir(details)
 //         fetch('http://127.0.0.1')
 //     }, { urls: ["<all_urls>"] });
-
-chrome.webRequest.onSendHeaders.addListener(
-    (details) => {
-        // method = details.method;
-        rHeaders = details.requestHeaders;
-        console.log('sendHeaders');
-        console.dir(rHeaders);
-        type = details.type;
-        // match the given types
-        if (type in types) {
-            
-        }
-        // timeStamp = details.timeStamp;
-        // url = details.url;
-        console.dir(details)
-    }, requestFilters, ['requestHeaders', 'extraHeaders']
-)
 
 // function setListeners() {
 //     chrome.webRequest.onBeforeSendHeaders.addListener(requestHandler,
